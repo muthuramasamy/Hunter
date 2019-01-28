7747afa325292508a406930ca27fce6806b9930d
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-28 11:05:12 +0530

    Update repeatedseq_or_unique.py

--- repeatedseq_or_unique.py
+++ repeatedseq_or_unique.py
@@ -6,9 +6,9 @@ c=0
 s=set([i for i in l if l.count(i)>1 ])
 for i in s:
 	l2.append(i)
-	c=c+1
+	c=1
 l2.sort
-if c>1:
+if c==1:
 	for i in range(len(l2)):
 		if i==len(l2)-1:
 			print(l2[i],end='')
