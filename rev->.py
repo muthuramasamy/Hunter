d31b6170151a0e9e7a27ef1aa25b6a51a9390395
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-29 20:04:36 +0530

    Update rev->.py

--- rev->.py
+++ rev->.py
@@ -1,9 +1,11 @@
 # your code goes here
-n=int(input())
-li=list(map(int,input().split()))
+n = int(input(""))
+a = list(map(int,input().split()))
 for i in range(n-1,-1,-1):
-	if i==0:
-		print(li[i],end="")
-	else:
-		print(li[i],end="->")
+    if(i == 0):
+        print(a[i],end="")
+    else:
+        print(a[i],end="->")
+    
+
 	
