0073c23b6c17ae76eac754f375c4a21b38876b9d
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-02-12 20:32:01 +0530

    Update stringwise_rev.py

--- stringwise_rev.py
+++ stringwise_rev.py
@@ -1,8 +1,8 @@
 #your code goes here
 n=list(map(str,input().split()))
-for i in range(len(s)):
+for i in range(len(n)):
 	n[i]=n[i][::-1]
-for i in range(len(s)):
+for i in range(len(n)):
 	if i==len(n)-1:
 		print(n[i],end="")
 	else:
